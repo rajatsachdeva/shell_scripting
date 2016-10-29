0bcbd4e1687bfcd48e346389c674f46a21a754a5
rajatsachdeva <rajat.sachdeva621@gmail.com>
2016-10-29 13:51:10 +0530

    added while.sh

---
+++ chapter_2/while.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# shows the use of while loop
+
+# initialize a variable x to 1
+x=1
+
+# Here in the loop condition
+# the shell execute another command 
+# and accordingly if the command is
+# succesfull or not the loop continues.
+
+while       # start of loop
+    ((x<10))    # arithmetic comparision
+do          # start of loop body
+    echo loop $x
+    #date >date.$x
+    ((x=x+1))
+    #we can write two commands in the same line using ;
+    # echo loop $x; date >date.$x
+done        #end of loop
+
