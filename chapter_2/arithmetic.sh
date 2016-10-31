735b34b52b51a993127d4570aa9f7c48d9e93188
rajatsachdeva <rajat.sachdeva621@gmail.com>
2016-10-31 22:23:37 +0530

    updated arithmetic.sh

--- chapter_2/arithmetic.sh
+++ chapter_2/arithmetic.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+
+# set -x is used to enable the debugging
+# This makes possible to see that what 
+# all commands are being executed
+
+#n=$2
+
+set -x
+echo n before n++ is $n
+
 ((n++))
 if
 ((n>4||$1 == 0))
@@ -9,12 +19,28 @@ else
 echo in the else
 fi
 
-set -x
+#set -x
+# ** is exponentiation operator
+# n = 2**3 = 2 to power of 3  = 8
+# n = 13 = 1101
+
 ((n=2**3 + 5))
+
 ((y=n^4))
-echo y = $y  # prints 1, why?
+echo y = $y
+# y = n^4 = n XOR 4 = 13 XOR 4
+# y = 1101 XOR 0100
+# y = 1001 = 9
+
 ((y=n|4))
-echo y = $y  # prints 1, why?
+echo y = $y
+# y = n|4 = n OR 4 = 13 OR 4
+# y = 1101 OR 0100
+# y = 1101 = 13
+
 ((y=n&4))
-echo y = $y  # prints 1, why?
+echo y = $y
+# y = n&4 = n AND 4 = 13 AND 4
+# y = 1101 AND 0100
+# y = 0100 = 4
 
