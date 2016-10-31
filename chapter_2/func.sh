7a7fd674088a4d020d6fabd98e78d7f4a88d3129
rajatsachdeva <rajat.sachdeva621@gmail.com>
2016-11-01 01:14:36 +0530

    func.sh: updated the comments

--- chapter_2/func.sh
+++ chapter_2/func.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# The use of return in a function
+# it helps to return from a function
+# to the place it was being called from 
+# And we can capture the value of a function
 function myfunc {
     echo starting myfunc
-    return
+    return 0
     echo this is not executed
 }
 
 # call myfunc
-myfunc
+rc=`myfunc`
 
 # capturing the value from the function myfunc
 n=$(myfunc)
 
 echo n is $n
+echo rc is $rc
