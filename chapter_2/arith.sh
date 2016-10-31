afc9265224ac36653b21cad18ce9c6f8ee18d5ce
rajatsachdeva <rajat.sachdeva621@gmail.com>
2016-10-31 22:49:41 +0530

    updated arith.sh

--- chapter_2/arith.sh
+++ chapter_2/arith.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Here we assign the variable n 
+# via an arithmetic expression
+# n = 8 + 5
+# n = 13
 ((n=2**3 + 5))
 echo n = $n
+
+# Here XOR is used
+# y = 13 XOR 4
+# 13  1101
+#  4  0100
+#  y  1001 = 9 
 ((y=n^4))
 echo y = $y
