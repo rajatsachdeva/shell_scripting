450b69e4cb04555f5ea23e855da45d88a677e4e1
rajatsachdeva <rajat.sachdeva621@gmail.com>
2016-11-01 01:10:05 +0530

    case2.sh: to take message from CLI

--- chapter_2/case2.sh
+++ chapter_2/case2.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-echo -n "Print message? "
+echo -n "Enter the Message: "
+read msg
 valid=0
 
 while
    [ $valid == 0 ]
 do 
+    echo -n "Print the Message? :"
     read ans
     case $ans in
-    yes|YES|y|Y|y.*x ) echo will print the message
-                  echo The Message
+    yes|YES|y|Y|y*|Y* ) echo will print the message
+                  echo $msg
                   valid=1
                   ;;
-    [nN][oO] ) echo will not print the message
+    [nN][oO]|n|N ) echo will not print the message
                valid=1 ;;
     *        ) echo Yes or No of some form please ;;
     esac
