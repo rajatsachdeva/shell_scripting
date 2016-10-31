99dfb5a198a005aaaa626958b0c6c0483f63ff87
rajatsachdeva <rajat.sachdeva621@gmail.com>
2016-11-01 01:20:51 +0530

    udapted comments

--- chapter_2/here.sh
+++ chapter_2/here.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# use of redirection operator >>
+# use of redirection operator <<
+# shows the use of here
+# In this we take Mark or END something like that
+# tell the command that do your activity till
+# you encounter this mark/end/eof etc.
 
+# this the data presented till END is encountered
 sort <<END
 hi
 my
@@ -10,6 +15,8 @@ is Jack
 bye!
 END
 
+# In this we tell the loop to take the input 
+# till EOF is encountered
 while
 read a b c
 do 
