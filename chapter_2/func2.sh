b2ede059617664299d55387a9ef332888346bee1
rajatsachdeva <rajat.sachdeva621@gmail.com>
2016-10-31 22:28:38 +0530

    updated func2.sh

--- chapter_2/func2.sh
+++ chapter_2/func2.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# here we declare and define a function func2
+# in the body of func2 we call exit with and exit status value
+# This terminates not only the function func2 instead the whole process
+# is terminated and the exit status can checked using echo $?
+# on the command line
+
 function func2 {
     echo in func2
-    exit 2
-    echo more in func2
+    exit 2              # terminates the process with exit status as 2
+    echo more in func2 # will not be executed 
 }
 
 echo starting
 func2
-echo after func2
+echo after func2 # will not be executed
 
 # to check the exit status of func2
 # use echo $?
