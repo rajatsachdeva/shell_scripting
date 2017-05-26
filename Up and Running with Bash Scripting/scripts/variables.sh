c016b2e55d0fb6b7079bd2014694e0bc636f2457
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-05-26 22:19:47 +0530

    variables.sh

--- Up and Running with Bash Scripting/scripts/variables.sh
+++ Up and Running with Bash Scripting/scripts/variables.sh
@@ -15,3 +15,44 @@ echo $c
 # To use variables within a string use " "
 
 echo "$b!, I have $c apples."
+
+# Adding attribute to variables
+
+declare -i d=123 			# d is an (i)Integer
+
+echo "d is $d" 
+
+declare -r e=456			# e is (r) read-only, this means this variable cannot be modified
+
+echo "e is $e"
+
+declare -l f="LoWerCase" 	# f is lowercase, l is used to convert any arbitary string to lower case
+
+echo "f is %f"
+
+declare -u g="UpperCasE"	# g is UPPERCASE, u is used to convert any arbitart string to upper case
+
+echo "g is $g"
+
+# Built-in Variables
+# Get the home Directory
+echo $HOME
+
+# Get the present working directory similar 'pwd' command on command line
+echo $PWD
+
+# Get the machine type, can be used to get directory structure depending upon platform
+echo $MACHTYPE
+
+# Get the Host Name
+echo $HOSTNAME
+
+# Get the bash version
+echo $BASH_VERSION
+
+# Get the number of seconds of the bash session has run on prompt
+# but in script it will give seconds for the running time of script
+echo $SECONDS
+
+# Get the script name
+echo $0
