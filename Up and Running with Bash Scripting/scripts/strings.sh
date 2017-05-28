1ae34a0f6b7ec9cefb9e93e7d5328b3c197532ac
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-05-28 12:31:06 +0530

    strings

--- Up and Running with Bash Scripting/scripts/strings.sh
+++ Up and Running with Bash Scripting/scripts/strings.sh
@@ -16,5 +16,37 @@ echo ${#b}
 echo ${#c}
 
 # Slicing/Subtrings in Bash
+# {<string name> : <start position> : <step size>}
+# start is 0 by default
+# step is 1 by default
 d=${c:3}
 echo $d
+
+d=${c:3:4}
+echo $d
+
+d=${c: -4}
+echo $d
+
+d=${c: -4: 3}
+echo $d
+
+# Search and replace in a string
+# {string variable name/search string/replace string}
+fruit="apple banana banana cherry"
+echo $fruit
+echo ${fruit/banana/rajat}			# Only replaces the first instance
+echo ${fruit//banana/rajat}			# replaces all instances
+
+# To replace only if search string comes in the begining of the string
+# use # pound
+echo ${fruit/#apple/rajat}
+echo ${fruit/#banana/rajat}
+
+# To replace only if search string comes at the end of the string
+# use % percent
+echo ${fruit/%cherry/rajat}
+echo ${fruit/%banana/rajat}
+
+# we can also use matching
+echo ${fruit/c*/rajat}
