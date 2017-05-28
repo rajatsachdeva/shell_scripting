062583c8a2c2e892bbecdccaaa1b8ed4ff2c56bc
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-05-28 12:49:44 +0530

    text styling and coloring

--- Up and Running with Bash Scripting/scripts/text_styling_coloring.sh
+++ Up and Running with Bash Scripting/scripts/text_styling_coloring.sh
@@ -6,3 +6,35 @@
 # We can enable the escape sequences using -e option in echo command
 
 echo -e "\033[34;42mColor Text\033[0m"
+
+# Meaning of above expression
+# 'echo -e' enables the use of escape sequences
+
+# \033[34;42m   Escape sequence
+# 'm' indicates the end of escape sequence
+# 'Color Text' 	string to print out
+# \033[0m 		Reset the colors
+
+# Colored text [ANSI] 
+# Color		Foreground		Background
+# Black		30				40
+# Red		31				41
+# Green		32				42
+# Yellow	33				43
+# Blue		34				44
+# Magenta	35				45
+# Cyan		36				46
+# White		37				47
+
+# Some common examples
+echo -e "\033[37;40mWhite on Black\033[0m"
+
+echo -e "\033[30;47mBlack on white\033[0m"
+
+echo -e "\033[30;41mBlack on Red\033[0m"
+
+echo -e "\033[32;40mGreen on Black\033[0m"
+
+echo -e "\033[31;47mRed on white\033[0m"
+
+echo -e "\033[34;43mBlue on Yellow\033[0m"
