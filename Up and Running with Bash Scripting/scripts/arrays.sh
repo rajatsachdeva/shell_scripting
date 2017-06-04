d6e52c02f8c898b72ce5d712e2a12d051895ae01
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2017-06-04 23:41:29 +0530

    arrays

---
+++ Up and Running with Bash Scripting/scripts/arrays.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Arrays in Bash
+
+a=()
+b=("apple" "banana" "cherry")
+
+echo ${b[2]}
+
+# adding element at any position
+# Sparse arrays
+b[5]="kiwi"
+
+# append at end of array
+b+=("mango")
+
+# Print the array
+echo ${b[@]}
+
+# Print the last element using slicing
+echo ${b[@] : -1}
+
+# Associative Arrays
+# key and values
+# if your key/value has a space in it use quotes ""
+declare -A myarray 
+myarray[color]=blue
+myarray["office building"]="HQ West"
+
+echo ${myarray["office building"]} is ${myarray[color]}
