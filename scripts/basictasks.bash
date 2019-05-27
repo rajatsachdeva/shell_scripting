3a0bf720fa6ed3df77cf1464b039bd2af08d8f0b
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-05-28 00:32:10 +0530

    updated basic tasks script

---
+++ scripts/basictasks.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Author: Rajat Sachdeva
+# Date Created: 28/05/2019
+# Description: Run basic linux tasks
+# Date Modified: 28/05/2019
+
+echo 
+echo This script will run a few basic tasks
+echo 
+pwd 
+echo
+ls -ltr
+echo
+whoami
+echo
+date
+echo
+cal
+echo
+touch a b c
+echo End of script
