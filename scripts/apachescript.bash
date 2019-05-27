d969ae93f2447c650d92fa77b0445afaa2899ca8
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-05-28 00:22:25 +0530

    updated scripts

---
+++ scripts/apachescript.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Purpose: Testing script format
+# Date: 25/05/2019
+# Modification: 27/05/2019
+# Test comment
+
+# define a variable a 
+a='My name is Rajat'
+
+# Run Commands
+echo $a
+
+date 
+df -h
+
+# for statement formatting its better to use tab
+# before writing statement to have better indentation
+
+    if { $a -eq file }
+    then echo this
+    else 
+        echo that 
+    fi
+
--- scripts/helloworld.bash
+++ scripts/helloworld.bash
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Author: Rajat Sachdeva
+# Date Created: 28/05/2019
+# Description: This script will output "Hello, World!" 
+# Date Modified: 28/05/2019
+
+echo
 echo "Hello, World!"
+echo
