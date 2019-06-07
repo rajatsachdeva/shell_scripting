6600a7c97c2748f858d4781b32f014ada554bf85
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-06-08 00:55:38 +0530

    added scripts

---
+++ professional-scripts/pull-warnings.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Author: Rajat Sachdeva
+# Date Created: 08/06/2019
+# Description: This script will pull only Error messages from /var/log/system.log
+# Date Modified: 08/06/2019
+
+grep -i warn /Users/rohanrajat/Documents/Bash_Scripting/shell_scripting/professional-scripts/system.log
