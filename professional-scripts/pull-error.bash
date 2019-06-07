1384cfc9475ea27be580ead6417c8d8d6ac65d44
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-06-08 01:02:12 +0530

    updated scripts

--- professional-scripts/pull-error.bash
+++ professional-scripts/pull-error.bash
@@ -4,4 +4,4 @@
 # Description: This script will pull only Error messages from /var/log/system.log
 # Date Modified: 08/06/2019
 
-grep -i error /Users/rohanrajat/Documents/Bash_Scripting/shell_scripting/professional-scripts/system.log
+grep -i error /Users/rohanrajat/Documents/Bash_Scripting/shell_scripting/professional-scripts/system.log 2> /Users/rohanrajat/Documents/Bash_Scripting/shell_scripting/professional-scripts/output-error.log
