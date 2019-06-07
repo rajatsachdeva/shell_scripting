00229d395da4a662f3b0cf493333d51b3df70dba
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-06-08 00:58:10 +0530

    updated scripts

---
+++ professional-scripts/pull-fail.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Author: Rajat Sachdeva
+# Date Created: 08/06/2019
+# Description: This script will pull only Fail messages from /var/log/system.log
+# Date Modified: 08/06/2019
+
+grep -i fail /Users/rohanrajat/Documents/Bash_Scripting/shell_scripting/professional-scripts/system.log
