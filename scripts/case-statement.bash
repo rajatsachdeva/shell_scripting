057d16daa8272924175895137cff9885b42b21a9
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-06-09 00:10:34 +0530

    Update case-statement.bash

--- scripts/case-statement.bash
+++ scripts/case-statement.bash
@@ -26,6 +26,21 @@ case $choices in
     *) echo Invalid choice - Bye.
 esac
 
+# This script will look at your current day and tell you the state of the backup
+
+NOW=$(date +"%a")
+case $NOW in 
+    Mon)
+        echo "Full backup";;
+    Tue|Wed|Thu|Fri)
+        echo "Partial backup";;
+    Sat|Sun)
+        echo "No Backup";;
+    *) ;;
+esac
+
+
+
 echo
 echo End of Script
 echo
