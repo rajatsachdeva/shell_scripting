754e5f537a11697f19862ece2eb8af824d6194e1
Rajat Sachdeva <rajat.sachdeva621@gmail.com>
2019-06-09 00:18:34 +0530

    Update for-loop.bash

--- scripts/for-loop.bash
+++ scripts/for-loop.bash
@@ -48,4 +48,15 @@ do
     echo "Weekday $((i++)) : $day"
 done
 echo
+
+# List all users one by one from /etc/passwd file 
+
+i=1
+for username in `awk -F: '{print $1}' /etc/passwd`
+    do 
+        echo "Username $((i++)) : $username"
+    done
+
+echo 
+
 echo End of Script
